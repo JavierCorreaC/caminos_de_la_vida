58b9130543f4d93a95b52045b2f0329f70b6cc68
jajaCorrea <40904957+jajaCorrea@users.noreply.github.com>
2020-01-24 18:28:53 -0600

    first commit

---
+++ src/ia_carretera/IA_Carretera.java
@@ -0,0 +1,25 @@
+package ia_carretera;
+
+import java.lang.Math;
+import java.util.Random;
+/**
+ *
+ * @author javier
+ */
+public class IA_Carretera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //int caminos = (int)(Math.random()*100+1);
+        int caminos = 3;
+        int numero = (int)(Math.random()*100+1);
+        System.out.println(numero);
+        float[] SUITS = new float[3];
+        
+        
+        float[][]  matriz = {{3},SUITS,{}};
+    }
+    
+}
