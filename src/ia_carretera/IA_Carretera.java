1022b81517211b64ca166663d3ae99f75d4b1ea3
crasher9032 <51750818+crasher9032@users.noreply.github.com>
2020-01-25 18:51:20 -0600

    prueba

--- src/ia_carretera/IA_Carretera.java
+++ src/ia_carretera/IA_Carretera.java
@@ -2,24 +2,26 @@ package ia_carretera;
 
 import java.lang.Math;
 import java.util.Random;
-/**
- *
- * @author javier
- */
-public class IA_Carretera {
 
-    /**
-     * @param args the command line arguments
-     */
+public class IA_Carretera {
     public static void main(String[] args) {
         //int caminos = (int)(Math.random()*100+1);
         int caminos = 3;
-        int numero = (int)(Math.random()*100+1);
-        System.out.println(numero);
+        int secciones = (int)(Math.random()*100+1);
         float[] SUITS = new float[3];
-        
-        
-        float[][]  matriz = {{3},SUITS,{}};
+        float[][][]  matriz = new float[caminos][secciones][3];
+        for (float[][] fs : matriz) {
+            for (float[] f : fs) {
+                generar(fs);
+            }
+        }
     }
-    
+    public static void generar(float[][] seccion){
+        try{
+            
+            System.out.println(seccion);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
